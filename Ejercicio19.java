825e3ee987b5700b7bd600dfb8b0382e7f08e519
jotaeme890 <permanentjavi@gmail.com>
2023-02-22 09:05:15 +0100

    Cambio hecho

--- Ejercicio19.java
+++ Ejercicio19.java
@@ -2,7 +2,7 @@
  * Une y amplía los dos programas anteriores de tal forma que se permita
  * convertir un número entre cualquiera de las siguientes bases: decimal, binario,
  * hexadecimal y octal.
- * Si ves esto vas bien!
+ * ¡Si ves esto vas bien!
  * No puedo mas de verdad me desespero
  * Vuenas tardes
  * Pruuuuuu
