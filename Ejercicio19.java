43b61b5b2aa6c5759eb3d34772d3475b8cfe3708
Adrián Perogil Fernández <chopon007@gmail.com>
2023-02-23 18:54:16 +0100

    Hola

--- Ejercicio19.java
+++ Ejercicio19.java
@@ -1,6 +1,7 @@
 /**
  * Une y amplía los dos programas anteriores de tal forma que se permita
  * convertir un número entre cualquiera de las siguientes bases: decimal, binario,
+ * Hola
  * hexadecimal y octal.
  * @author: Javier Miguel Martín Gallardo
  */
